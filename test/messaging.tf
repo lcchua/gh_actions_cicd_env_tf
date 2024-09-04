590cfea47c9f70c68e263035f3712634fe350135
lcchua <laichwang@gmail.com>
2024-09-04 16:43:10 +0800

    Correction var.env_type

--- test/messaging.tf
+++ test/messaging.tf
@@ -10,5 +10,5 @@ resource "aws_sns_topic" "user_updates" {
 
 output "sns_topic_arn" {
   value       = aws_sns_topic.user_updates.arn
-  description = "The ARN of the SNS topic in ${var.en_type}"
+  description = "The ARN of the SNS topic in ${var.env_type}"
 }
