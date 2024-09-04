8d918d2b3708499e62e0604ca59dce6b78d3eb07
lcchua <laichwang@gmail.com>
2024-09-04 17:09:29 +0800

    Corrected tfstate backend to point to the same s3 for dev and test env

--- dev/backend.tf
+++ dev/backend.tf
@@ -13,7 +13,7 @@ terraform {
   # terraform state file is locally stored
   backend "s3" {
     bucket = "sctp-ce7-tfstate"
-    key    = "terraform-ex-gh-action-lcchua.tfstate"
+    key    = "tf-gh-action-lcchua-dev.tfstate"
     region = "us-east-1"
   }
 }
