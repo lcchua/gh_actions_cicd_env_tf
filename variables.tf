8f78570d9c7e5899587088b20b4db6819b21e421
lcchua <laichwang@gmail.com>
2024-09-05 09:51:18 +0800

    Debugging variables.tf fmt

--- variables.tf
+++ variables.tf
@@ -4,18 +4,14 @@ variable "stack_name" {
   type    = string
   default = "lcchua-STW"
 }
-*/
 
-/* 
 variable "key_name" {
   description = "Name of EC2 Key Pair"
   type        = string
   default     = "lcchua-useast1-20072024"
   default       = "lcchua-useast1-30072024"
 }
-*/
 
-/*
 variable "working_dir" {
   description = "Pathname of my local working directory"
   type        = string
